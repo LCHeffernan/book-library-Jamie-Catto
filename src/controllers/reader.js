73dd2cb57c8ced933c04e33e0a08e563f1841081
Jamie Catto <jamietcatto@gmail.com>
2023-01-11 10:12:47 +0000

    Add reader now creating table records.

--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -1,5 +1,6 @@
 const { Reader } = require("../models");
 
-exports.addReader = (req, res) => {
-    res.sendStatus(201);
+exports.addReader = async (req, res) => {
+    const newReader = await Reader.create(req.body);
+    res.status(201).json(newReader);
 };
--- tests/reader.test.js
+++ tests/reader.test.js
@@ -14,11 +14,16 @@ describe('/readers', () => {
     describe('POST /readers', () => {
       it('creates a new reader in the database', async () => {
         const response = await request(app).post('/readers').send({
-          name: 'Elizabeth Bennet',
-          email: 'future_ms_darcy@gmail.com',
+          name: 'Benedict Cumberbatch',
+          email: 'benny.cumbo@gmail.com',
         });
 
+        const newReaderRecord = await Reader.findByPk(response.body.id, { raw: true });
+        console.log(response.body);
         expect(response.status).to.equal(201);
+        expect(response.body.name).to.equal('Benedict Cumberbatch');
+        expect(newReaderRecord.name).to.equal('Benedict Cumberbatch');
+        expect(newReaderRecord.email).to.equal('benny.cumbo@gmail.com');
       });
     });
   });
